e33cbf28c6736ffef7a8e7b7eb2471cd74a5070c
panpan <panpan31415@gmail.com>
2024-07-20 14:30:50 +0200

    create EventList

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,9 +1,30 @@
 import H1 from "@/components/EventHeader";
+import { Event } from "@/lib/types";
 
-export default function CityEvents() {
+type CityEventsPageProps = {
+    params: {
+        city: string;
+    };
+};
+const EVENTS_API_URL = "https://bytegrad.com/course-assets/projects/evento/api/events";
+
+export default async function CityEventsPage({ params }: CityEventsPageProps) {
+    const { city } = params;
+    let headerText = "";
+    if (city.trim() === "all") {
+        headerText = "All events";
+    } else {
+        headerText = "Events in " + city.charAt(0).toUpperCase() + city.slice(1);
+    }
+
+    const response = await fetch(`${EVENTS_API_URL}?city=${city}`);
+    const events: Event[] = await response.json();
     return (
         <main className='flex flex-col items-center py-24 px-[20px] min-h-[110vh]'>
-            <H1>City Event Header</H1>
+            <H1>{headerText}</H1>
+            {events.map((event) => (
+                <section key={event.id}> {event.name}</section>
+            ))}
         </main>
     );
 }
---
+++ src/components/EventList.tsx
@@ -0,0 +1,8 @@
+import { Event } from "@/lib/types";
+
+type EventListProps = {
+    events: Event[];
+};
+export default function EventList({ events }: EventListProps) {
+    return <div></div>;
+}
