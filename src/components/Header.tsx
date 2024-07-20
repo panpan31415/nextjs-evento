c8ce154e466fcaaa78f9ff40ce7949f8b137f937
panpan <panpan31415@gmail.com>
2024-07-20 11:33:30 +0200

    add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
+"use client";
 import Link from "next/link";
 import Logo from "./Logo";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+import { motion } from "framer-motion";
 
 const routes = [
     { path: "/", name: "Home" },
@@ -7,17 +11,31 @@ const routes = [
 ];
 
 export default function Header() {
+    const activePath = usePathname();
+
     return (
         <header className='w-full h-14   border-b border-white/10 flex justify-between items-center px-3 md:px-9'>
             <Logo />
-            <nav>
-                <ul className='flex gap-6 text-sm'>
+            <nav className='h-full'>
+                <ul className='h-full flex items-center  gap-6 text-sm '>
                     {routes.map((route) => (
-                        <li key={route.name} className="text-white/50 hover:text-white transition">
+                        <li
+                            key={route.name}
+                            className={clsx(
+                                "hover:text-white transition  items-center relative flex h-full items-center",
+                                {
+                                    "text-white/100": activePath === route.path,
+                                    "text-white/50": activePath !== route.path,
+                                },
+                            )}>
                             <Link href={route.path}>{route.name}</Link>
+                            {activePath === route.path && (
+                                <motion.div
+                                    layoutId='activePath'
+                                    className='bg-accent h-1 w-full absolute bottom-0 '></motion.div>
+                            )}
                         </li>
                     ))}
-
                 </ul>
             </nav>
         </header>
