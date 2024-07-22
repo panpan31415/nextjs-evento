edc0b7863092a945ac375f00ea2a3c084f93aafb
panpan <panpan31415@gmail.com>
2024-07-22 16:03:57 +0200

    rm sleep

--- src/components/event-list.tsx
+++ src/components/event-list.tsx
@@ -1,6 +1,6 @@
 import { Event } from "@/lib/types";
 import EventCard from "./event-card";
-import { sleep } from "@/lib/utils";
+// import { sleep } from "@/lib/utils";
 
 const EVENTS_API_URL = "https://bytegrad.com/course-assets/projects/evento/api/events";
 
@@ -8,11 +8,11 @@ type EventListProps = {
     city: string;
 };
 export default async function EventList({ city }: EventListProps) {
-    await sleep(2000);
+    // await sleep(2000);
     const response = await fetch(`${EVENTS_API_URL}?city=${city}`);
     const events: Event[] = await response.json();
     return (
-        <section className='w-full flex flex-wrap gap-10 justify-center'>
+        <section className='flex flex-wrap gap-10 justify-center'>
             {events.map((event) => (
                 <EventCard
                     event={event}
