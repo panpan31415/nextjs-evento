f17c06433a72d3fa0d79efb7f727009f8bb8fcec
panpan <panpan31415@gmail.com>
2024-07-25 13:43:01 +0200

    rename git cache for container.tsx

--- src/components/Container.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from "react";
-
-type ContainerProps = {
-    children: ReactNode;
-};
-export default function Container({ children }: ContainerProps) {
-    return <div className='max-w-7xl mx-auto bg-white/[2%] min-h-screen flex flex-col '>{children}</div>;
-}
